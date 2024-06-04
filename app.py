cebb585286653c2c047787b76a1b9d1ec86c1f40
Geovanna <geowalves@gmail.com>
2024-06-04 18:35:21 -0300

    Adicionando algumas funções importantes

--- app.py
+++ app.py
@@ -5,30 +5,54 @@ import pytz
 from winotify import Notification
 import tkinter as tk
 from tkinter import ttk
-
-# link do open_weather: https://openweathermap.org/
-
+from datetime import datetime
 
 
-# CHAMADA DE API E APRESENTAÇÃO DAS INFORMAÇÕES
-API_KEY = "1e95d8e167c2fd18818bb85e0d6fc4e0"
-cidade = input("Digite o nome da cidade: ")
-link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
 
-try:
-    requisicao = requests.get(link)
-    requisicao.raise_for_status()  # Verifica se a solicitação foi bem-sucedida
-    requisicao_dic = requisicao.json()
+# Obter informações do clima 
 
-    descricao = requisicao_dic['weather'][0]['description']
-    temperatura = requisicao_dic['main']['temp'] - 273.15
-    print(f"Previsão para {cidade}: {descricao}, temperatura: {temperatura:.2f}°C")
+def obter_clima(cidade):
+    API_KEY = "1e95d8e167c2fd18818bb85e0d6fc4e0"
+    link = f"https://api.openweathermap.org/data/2.5/weather?q={cidade}&appid={API_KEY}&lang=pt_br"
+
+    try:
+        requisicao = requests.get(link)
+        requisicao.raise_for_status()
+        requisicao_dic = requisicao.json()
+
+        descricao = requisicao_dic['weather'][0]["description"]
+        temperatura = requisicao_dic['main']['temp'] -273.15
+        umidade = requisicao_dic['main']['humidity']
+        vento = requisicao_dic['wind']['speed']
+
+
+        return descricao, temperatura, umidade, vento
+    except requests.exceptions.RequestException as e:
+        return None, None, None, None
     
-except requests.exceptions.RequestException as e:
-    print("Erro ao fazer a solicitação:", e)
-except KeyError as e:
-    print("Erro ao analisar os dados do clima:", e)
+
+# Atualizar e exibir informações do clima
+def atualizar_clima():
+    cidade = cidade_entry.get()
+    descricao, temperatura, umidade, vento = obter_clima
+
+    if descricao:
+        clima_info.set(f"Localização {cidade}\n"
+                       f"Descrição: {descricao.capitalize()}\n"
+                       f"Temperatura: {temperatura:.2f}°C\n"
+                       f"Umidade: {umidade}%\n"
+                       f"Vento: {vento} m/s")
+        
+    else:
+        clima_info.set("Não foi possível obter as informações do clima.")
+
+# Atualizar data e hora
+def atualizar_data_hora():
+    agora = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    data_hora.set(f"Data e Hora: {agora}")
+    root.after(1000, atualizar_data_hora)
 
 
-
-
+# Configurações da interface gráfica
+root = tk.Tk()
+root.title("Aplicativo de Previsão do tempo")
